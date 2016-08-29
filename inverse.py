97e373aa1615ed5904a667dd36d49aa156a1d3c4
anhsirkrishna <krishna_sp@outlook.com>
2016-08-29 14:41:13 +0530

    Added Main funtion so it can be imported

--- inverse.py
+++ inverse.py
@@ -6,36 +6,41 @@ def GCD(a,b):
 	
 	return GCD(b,a%b)
 	
+def eu_inverse(b,m):
+	A = [1,0,m]
+	B = [0,1,b]
+	T = [0,0,0]
 
-parser =  argparse.ArgumentParser()
-parser.add_argument("b", help="Enter b, the number to find the inverse of")
-parser.add_argument("m", help="Enter m, the number relative to the inverse")
-args = parser.parse_args()
-
-b = int(args.b)
-m = int(args.m)
-
-A = [1,0,m]
-B = [0,1,b]
-T = [0,0,0]
-
-while(1):
-	if(B[2] == 0):
-		print("There is no inverse")
-		break
-	
-	if(B[2] == 1):
-		bi = B[1]%m
-		print("Inverse of ",b," w.r.t ",m," is ",bi)
-		break
+	while(1):
+		if(B[2] == 0):
+			return null
+			#print("There is no inverse")
+			break
 		
-	Q = int(A[2]/B[2])
-	print("Q = ",Q)
+		if(B[2] == 1):
+			bi = B[1]%m
+			return bi
+			#print("Inverse of ",b," w.r.t ",m," is ",bi)
+			break
+			
+		Q = int(A[2]/B[2])
+		print("Q = ",Q)
+		
+		for i in range(3):
+			T[i] = A[i] - (Q * B[i])
+			A[i] = B[i]
+			B[i] = T[i]
+		
+		#print("T = ",T)
+		#print("________________________")
+
+if __name__ == "__main__":
+	parser =  argparse.ArgumentParser()
+	parser.add_argument("b", help="Enter b, the number to find the inverse of")
+	parser.add_argument("m", help="Enter m, the number relative to the inverse")
+	args = parser.parse_args()
+
+	b = int(args.b)
+	m = int(args.m)
 	
-	for i in range(3):
-		T[i] = A[i] - (Q * B[i])
-		A[i] = B[i]
-		B[i] = T[i]
-	
-	print("T = ",T)
-	print("________________________")
+	eu_inverse(b,m)
